664e2506c4a4ae714b18fbc8d4039864bad8464d
Billiam <billiamthesecond@gmail.com>
2022-05-22 23:26:31 -0500

    Block context menu for touch devices

---
+++ src/components/ContextMenu.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { onBeforeMount } from 'vue'
+
+onBeforeMount(() => {
+  window.addEventListener('contextmenu', (e) => {
+    if (e.pointerType !== 'mouse') {
+      e.preventDefault()
+      e.stopPropagation()
+    }
+  })
+})
+</script>
+<template></template>
