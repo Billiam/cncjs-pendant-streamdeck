34255b3a4d9b15531be239e07a1c88a843301e2a
Billiam <billiamthesecond@gmail.com>
2022-05-22 20:25:52 -0500

    Add component for mobile view height styling

---
+++ src/components/FixedHeight.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { onBeforeMount, onBeforeUnmount } from 'vue'
+
+const setHeight = () => {
+  const vh = window.innerHeight * 0.01
+  document.documentElement.style.setProperty('--vh', `${vh}px`)
+}
+onBeforeMount(() => {
+  setHeight()
+  window.addEventListener('resize', setHeight)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', setHeight)
+})
+</script>
+<template></template>
