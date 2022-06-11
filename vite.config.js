f6b62e052356c4d37874c68a3857a83b87d3f15b
Billiam <billiamthesecond@gmail.com>
2022-06-11 18:39:48 -0500

    Allow localhost proxying in development

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,32 @@
 import { fileURLToPath, URL } from 'url'
+import { defineConfig, loadEnv } from 'vite'
 
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  root: 'src',
-  resolve: {
-    alias: {
-      vue: 'vue/dist/vue.esm-bundler',
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const config = {
+    plugins: [vue()],
+    root: 'src',
+    server: {},
+    resolve: {
+      alias: {
+        vue: 'vue/dist/vue.esm-bundler',
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
+  }
+
+  if (env.PROXY_URL) {
+    config.server.proxy = {
+      '/api': {
+        target: env.PROXY_URL,
+        changeOrigin: true,
+      },
+    }
+  }
+
+  return config
 })
