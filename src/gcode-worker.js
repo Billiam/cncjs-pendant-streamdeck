c295ea4c725cc517bf8d076198cbc3432a0ecb56
Billiam <billiamthesecond@gmail.com>
2022-06-12 11:23:17 -0500

    Strip brackets and percents from gcode

--- src/gcode-worker.js
+++ src/gcode-worker.js
@@ -1,7 +1,9 @@
 import { parse } from '@/vendor/gcodetogeometry/gcodetogeometry'
 
 self.addEventListener('message', (event) => {
-  const gcode = event.data.gcode.replace(/^%.*/gm, '')
+  // stripe lines beginning with % or containing [
+  const gcode = event.data.gcode.replace(/^%.*/gm, '').replace(/^.*\[.*/gm, '')
+
   self.postMessage({
     name: event.data.name,
     geometry: parse(gcode),
