70cf16f120308b7fea44c7623c51c4d9cd66c71d
Billiam <billiamthesecond@gmail.com>
2022-07-08 01:09:50 -0500

    Fix chained image operations

--- src/lib/cli/button-renderer.js
+++ src/lib/cli/button-renderer.js
@@ -1,7 +1,7 @@
 import sharp from 'sharp'
 const cache = {}
 
-const size = 144
+const inputSize = 144
 
 const textOffset = (alignment, width) => {
   switch (alignment) {
@@ -23,6 +23,7 @@ const textVOffset = (alignment, height, textHeight) => {
       return height - textHeight - 4
   }
 }
+
 const getRender = async (config) => {
   if (!config.show) {
     return []
@@ -30,13 +31,13 @@ const getRender = async (config) => {
 
   const columns = config.columns ?? 1
   const rows = config.rows ?? 1
-  const outputSize = 72
+  const outputSize = config.size ?? 72
 
-  const width = columns * size
-  const height = rows * size
+  const inputWidth = columns * inputSize
+  const inputHeight = rows * inputSize
   const outputWidth = columns * outputSize
   const outputHeight = rows * outputSize
-  const createConfig = { width, height, channels: 4 }
+  const createConfig = { width: inputWidth, height: inputHeight, channels: 4 }
   if (config.color) {
     createConfig.background = config.color
   }
@@ -45,17 +46,20 @@ const getRender = async (config) => {
     create: createConfig,
   })
   const composite = []
-  const lateComposite = []
-  // TODO: push overlays in parallel
+  const finalComposite = []
+
   if (config.icon) {
     const overlay = await sharp(`src/public/icons/${config.icon}`)
       .resize({
-        width,
-        height,
+        width: inputWidth,
+        height: inputHeight,
         fit: 'contain',
+        kernel: 'cubic',
       })
       .toBuffer()
-    composite.push({ input: overlay })
+    composite.push({
+      input: overlay,
+    })
   }
 
   if (config.text) {
@@ -78,7 +82,7 @@ const getRender = async (config) => {
     )
 
     // TODO: Don't hardcode width/height
-    const svg = `<svg width="${outputWidth}" height="${outputHeight}">
+    const svg = `<svg width="${outputWidth}px" height="${outputHeight}px">
       <text
         fill="${config.cellTextColor}"
         font-size="${fontSize}px"
@@ -90,51 +94,58 @@ const getRender = async (config) => {
     </svg>`
 
     const textBuffer = Buffer.from(svg)
-    lateComposite.push({ input: textBuffer })
+    finalComposite.push({ input: textBuffer, top: 0, left: 0 })
   }
-
-  if (composite.length) {
-    image = image.composite(composite)
-  }
-  const buffer = await image.raw().toBuffer()
 
   if (!config.enabled) {
-    lateComposite.push({
+    finalComposite.push({
       input: Buffer.from([0, 0, 0, 180]),
       raw: { width: 1, height: 1, channels: 4 },
       tile: true,
     })
   }
-  const resizeImage = sharp(buffer, {
-    raw: { width: size, height: size, channels: 4 },
-  })
-    .composite(lateComposite)
-    .resize(outputWidth, outputHeight, { kernel: 'cubic' })
-    .raw()
+
+  if (composite.length) {
+    image = sharp(await image.composite(composite).raw().toBuffer(), {
+      raw: { width: inputWidth, height: inputHeight, channels: 4 },
+    })
+  }
+
+  image = image
+    .resize({
+      width: outputWidth,
+      height: outputHeight,
+      kernel: 'cubic',
+    })
+    .composite(finalComposite)
 
   if (columns > 1 || rows > 1) {
     const grid = []
+
+    // new operation before resize following a composition
+    if (finalComposite.length) {
+      image = sharp(await image.toBuffer())
+    }
+
     for (let i = 0; i < columns; i++) {
       for (let j = 0; j < rows; j++) {
         grid.push(
-          resizeImage
+          await image
             .extract({
               left: i * outputSize,
               top: j * outputSize,
               width: outputSize,
               height: outputSize,
             })
+            .raw()
             .toBuffer()
         )
       }
     }
-
     return Promise.all(grid)
   }
-
-  const outputBuffer = await resizeImage.toBuffer()
+  const outputBuffer = await image.raw().toBuffer()
   return [outputBuffer]
-  // return Promise.all([image.toBuffer()])
 }
 
 const createObjectKey = (config) => {
