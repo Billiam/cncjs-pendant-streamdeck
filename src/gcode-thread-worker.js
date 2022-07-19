c530eeb812a80da4e63354d04380c32c0467c71b
Billiam <billiamthesecond@gmail.com>
2022-07-19 00:47:48 -0500

    Allow total line limit in gcode parsing worker

--- src/gcode-thread-worker.js
+++ src/gcode-thread-worker.js
@@ -1,11 +1,24 @@
 import { parse } from '@/vendor/gcodetogeometry/gcodetogeometry'
 const parentPort = require('worker_threads').parentPort
 
+const stringLimit = (str, limit) => {
+  const l = str.length
+  let i = -1
+  const newline = '\n'
+  while (limit-- && i++ < l) {
+    i = str.indexOf(newline, i)
+    if (i < 0) return str
+  }
+  return str.substring(0, i)
+}
 // some code duplication here compared to the webpack worker due to vite+rollup
 // limitations
 parentPort.on('message', (event) => {
   // strip lines beginning with % or containing [
-  const gcode = event.gcode.replace(/^%.*/gm, '').replace(/^.*\[.*/gm, '')
+  let gcode = event.gcode
+  if (event.limit) {
+    gcode = stringLimit(gcode, event.limit)
+  }
   parentPort.postMessage({
     data: {
       name: event.name,
